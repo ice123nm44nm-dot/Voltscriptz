539dd0d57e16967198cc0ec8f0fba0d01f463199
ice123nm44nm-dot <ice123nm44nm@gmail.com>
2026-08-18 00:24:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const scripts = [
     status: "KEY SCRIPT",
     author: "@VOLTSCRIPTZ",
     tags: ["KEY"],
-    script: `loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4c2fde7fb17b25f6455bcc2c7a9110b7b5e46048cda8ff9832722af290ac5d08/download"))()`
+    script: `loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/c83ba9fc1eb53ca179337555be7a6d546cecf15effafd8ae432e9b02e3581f3b/download"))()`
   },
 {
     name: "San Diego Border Roleplay",
