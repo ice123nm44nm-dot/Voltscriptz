77c9d399a8ea831d3e76f88de060b40f6088d84e
ice123nm44nm-dot <ice123nm44nm@gmail.com>
2026-08-25 13:17:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,15 @@
 */
 const scripts = [
   {
+    name: "Clean All Leaves",
+    game: "Clean All Leaves",
+    image: "images/Clean.png",
+    status: "KEY SCRIPT",
+    author: "@VOLTSCRIPTZ",
+    tags: ["KEY"],
+    script: `loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/c83ba9fc1eb53ca179337555be7a6d546cecf15effafd8ae432e9b02e3581f3b/download"))()`
+  },
+  {
     name: "Ghost Driver",
     game: "Ghost Driver",
     image: "images/GHOSTDRIVER.png",
